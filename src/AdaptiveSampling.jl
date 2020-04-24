53acd2fc067c8331b97353d0c77d3228c3d0f367
iuliancioarca <iulian.cioarca@gmail.com>
2020-04-24 13:44:35 +0300

    initial commit

---
+++ src/AdaptiveSampling.jl
@@ -0,0 +1,5 @@
+module AdaptiveSampling
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AdaptiveSampling
+using Test
+
+@testset "AdaptiveSampling.jl" begin
+    # Write your own tests here.
+end
